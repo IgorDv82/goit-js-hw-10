9de187943c8a9be9a5f1e6a83b75bcdc48ea2296
Igor Dorianskyi <dis.igor@gmail.com>
2023-06-05 01:17:39 +0300

    complete with rendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const selectEl = document.querySelector('.breed-select');
-const info = document.querySelector('.cat-info');
+const infoEl = document.querySelector('.cat-info');
+const loaderEl = document.querySelector('.loader');
+infoEl.setAttribute('style', 'width: 100%; display: flex; gap: 15px ');
 
-fetchBreeds().then(data => renderOptions(data));
+fetchBreeds().then(data => renderOptionToSelect(data));
 
-function renderOptions(data) {
+function renderOptionToSelect(data) {
   selectEl.innerHTML = data
     .map(({ id, name }) => `<option value="${id}">${name}</option>`)
     .join('');
@@ -14,11 +16,16 @@ function renderOptions(data) {
 function onInput(evt) {
   const catId = evt.currentTarget.value;
   fetchCatByBreed(catId).then(data => {
-    console.log(data[0]);
+    createMarkup(...data);
   });
 }
 
-function createMarkup(arr) {
-  console.log(arr);
+function createMarkup({ url, breeds }) {
+  infoEl.innerHTML = breeds
+    .map(
+      ({ description, name }) =>
+        `<div><img class="image" src="${url}" alt="cat ${name}" style="max-width: 480px"></div><div><h2>${name}</h2><p class="info">${description}</p></div>`
+    )
+    .join('');
 }
 selectEl.addEventListener('input', onInput);
